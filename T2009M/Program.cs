0ae85f8ffaf20d81705cdd5407e8ed5508c00549
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-05-20 09:44:14 +0700

    up code

---
+++ T2009M/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace T2009M
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int a ;
+            string s;
+            Console.WriteLine("Nhap 1 chuoi tu ban phim:");
+            s = Console.ReadLine();// lay 1 dong vua nhap tu ban phim
+            Console.WriteLine("Nhap 1 so nguyen:");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("a = "+a);
+            // Console.WriteLine("Nhap 1 so thuc:");
+            // double b = Convert.ToDouble(Console.ReadLine());
+            if (CheckPrime(a))
+            {
+                Console.WriteLine(a+" la so nguyen to");
+            }
+        }
+
+        public static bool CheckPrime(int n)
+        {
+            if (n < 2) return false;
+            if (n < 4) return true;
+            for (int i = 2; i <= n / 2; i++)
+            {
+                if (n % i == 0) return false;
+            }
+
+            return true;
+        }
+    }
+}
