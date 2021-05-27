672ceafcf8813a973b027188d0baf9cb156beba6
hoatq-fptaptech <hoatq4@fpt.edu.vn>
2021-05-27 09:07:18 +0700

    demo delegate

---
+++ T2009M/session4/DemoDelegate.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace T2009M.session4
+{
+    public class DemoDelegate
+    {
+        public DemoDelegate()
+        {
+            Program.ss += ShowMsg;
+            Program.ss += SayHello;
+        }
+
+        public void SayHello(string s)
+        {
+            Console.WriteLine(s);
+        }
+        
+        public static void ShowMsg(string msg) // ham tra ve void va tham so la 1 string
+        {
+            Console.WriteLine("Show msg: "+msg);
+        }
+
+        public void ShowInfo(string info) // ham tra ve void va tham so la 1 string
+        {
+            Console.WriteLine("Info: "+info);
+        }
+    }
+}
